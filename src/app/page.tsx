05ff209a84e67ad6b47ad243d5cdea00cdd04a2d
vaibhav-hexalytics <101314569+vaibhav-hexalytics@users.noreply.github.com>
2024-03-01 15:12:06 +0530

    CSSLOad

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from 'primereact/button';
+
+
+
+
+export default function Home() {
+  return (
+    
+    <div className='space-y-[20px] p-[24px]'>
+      <div><Button label="Submit" /></div>
+      <div><Button label="Submit" /></div>
+      <div><Button label="Submit" /></div>
+    </div>
+  );
+}
+
